1d889995e6ea629c93666a0924d8a64f03c696c1
Mohammad Moiz Ali <moizprince786@gmail.com>
2024-12-01 17:14:58 +0500

    handle message stay bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import deleteOldMessages from "@/utils/deleteOlderMessages";
 
 const Header = () => {
+    useEffect(() => {
+        deleteOldMessages();
+    }, []);
     return (
         <header 
             className="bg-white/30 backdrop-blur-md text-center py-4 px-6 rounded-b-lg shadow-md w-full fixed top-0 left-0 z-10"
